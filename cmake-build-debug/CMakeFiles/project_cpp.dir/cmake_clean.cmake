97e2c0f4586910f46c9fbfbb3657cb47ce59e1f2
Clément <clement.robin@isen-ouest.yncrea.fr>
2025-04-01 11:32:21 +0200

    aa

---
+++ cmake-build-debug/CMakeFiles/project_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_cpp.dir/main.cpp.o"
+  "CMakeFiles/project_cpp.dir/main.cpp.o.d"
+  "project_cpp"
+  "project_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
